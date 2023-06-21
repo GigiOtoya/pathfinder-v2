4fc078d7ca7d345ec1a9036a009c88a1083572b9
Luigi Otoya <Luigi.Otoya@gmail.com>
2023-06-21 16:04:37 -0400

    small update

--- src/components/Controller.tsx
+++ src/components/Controller.tsx
@@ -1,23 +1,38 @@
 import './Controller';
 import './Controller.css'
 import { Navigation } from './Navigation';
+import PlayIcon from '../assets/PlayButton.svg';
+import PlusIcon from '../assets/plus-sign-icon.svg';
 
 export const Controller = () => {
     return (
         <div className="control-panel">
             <h1> Control Panel </h1>
             <button className="full-btn"> Drag </button>
-            <button className="half-btn"> Node </button>
-            <button className="half-btn"> Edge </button>
-            <button className="half-btn"> Play </button>
-            
-            <input 
-                type="range"
-                className="speed-control"
-                list="values"
-                min="0"
-                max="4"
-                step="1"></input>
+            <div className="side-by-side">
+                <button className="half-btn"> 
+                    <img src={ PlusIcon } alt="Add Icon" className="icon"/>
+                    Node 
+                </button>
+                <button className="half-btn"> 
+                    <img src={ PlusIcon } alt="Add Icon" className="icon"/>
+                    Edge 
+                </button>
+            </div>
+            <div className="side-by-side">
+                <button className="half-btn">
+                    <img src={ PlayIcon } alt="Play Icon" className="icon"/> 
+                    Play
+                </button>
+                
+                <input 
+                    type="range"
+                    className="speed-control"
+                    list="values"
+                    min="0"
+                    max="4"
+                    step="1"></input>
+            </div>
             <button className="full-btn"> Generate Random </button>
             <button className="full-btn"> Clear Graph </button>
             <Navigation/>
--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -2,7 +2,7 @@ import './Navigation.css'
 
 const algorithms: JSX.Element[] = [
     {id: 0, name: 'Dijkstra Shortest Path'},
-    {id: 1, name: 'FLoyd-Warshall (All-Pairs)'},
+    {id: 1, name: 'Floyd-Warshall (All-Pairs)'},
     {id: 2, name: 'Depth First Search (Recursive)'},
     {id: 3, name: 'Depth First Search (Iterative)'},
     {id: 4, name: 'Breadth First Search'},
